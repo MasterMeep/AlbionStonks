c17dd7c9ebbb0fb4f07fdcb5caa1fb56fcd5421a
MasterMeep <51376656+MasterMeep@users.noreply.github.com>
2022-04-26 11:08:15 -0500

    Create app.py

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ selected_city = st.selectbox("Select A City", sts.cities)
 selected_item = st.selectbox('Select An Item To Craft', sts.craftItems)
 with st.form(key='columns_in_form'):
     cols = st.columns(len(sts.craftRecipies[selected_item]))
-	num_of_inps = 0
+    num_of_inps = 0
     for i, col in enumerate(cols):
         col.number_input(f"Enter price of {sts.craftRecipies[selected_item][i]}", key=f"craft_inp_{i}")
-		num_of_inps += 1
+        num_of_inps += 1
     submittedCraft = st.form_submit_button('Submit')
 
 if submittedCraft:
