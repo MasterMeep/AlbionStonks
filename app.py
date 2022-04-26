061172a470bd46557f2ff188f903bc1b0ad35d1a
MasterMeep <51376656+MasterMeep@users.noreply.github.com>
2022-04-26 11:07:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,16 @@ selected_city = st.selectbox("Select A City", sts.cities)
 selected_item = st.selectbox('Select An Item To Craft', sts.craftItems)
 with st.form(key='columns_in_form'):
     cols = st.columns(len(sts.craftRecipies[selected_item]))
+	num_of_inps = 0
     for i, col in enumerate(cols):
         col.number_input(f"Enter price of {sts.craftRecipies[selected_item][i]}", key=f"craft_inp_{i}")
+		num_of_inps += 1
     submittedCraft = st.form_submit_button('Submit')
 
 if submittedCraft:
-	st.write(sts.craft_inp_0)
+	running = 0
+	for i in range(num_of_inps):	
+		running += int(getattr(sts, f"craft_inp_{i}"))
+
+	st.write(running)
 selected_item = st.selectbox('Select An Item To Refine', sts.refineItems)
