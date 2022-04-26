7c866adc2b6f4d770c30d4f410b1e5349f4b38c1
MasterMeep <51376656+MasterMeep@users.noreply.github.com>
2022-04-26 13:40:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if submittedCraft:
 	for i in (sts.craftRecipies[selected_item]):
 		sts.savedItems[i[0]] = int(getattr(sts, i[0]))
 		running += int(getattr(sts, i[0]))
-	sts.savedCraft[selected_item] = running
+	sts.savedCraft[selected_item] = {'Price': running}
 	st.table(sts.savedCraft)
 	
 
