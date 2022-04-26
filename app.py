3b428708c949cc2d36040e94a5d02a384699de49
MasterMeep <51376656+MasterMeep@users.noreply.github.com>
2022-04-26 11:30:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ with st.form(key='columns_in_form'):
     cols = st.columns(len(sts.craftRecipies[selected_item]))
     num_of_inps = 0
     for i, col in enumerate(cols):
-        try:col.number_input(f"Enter price of {sts.craftRecipies[selected_item][i][0]}", key=sts.craftRecipies[selected_item][i][0], step=1, value=getattr(sts, sts.craftRecipies[selected_item][i][0]))
+        try:col.number_input(f"Enter price of {sts.craftRecipies[selected_item][i][0]}", key=sts.craftRecipies[selected_item][i][0], step=1, value=getattr(sts.savedItems, sts.craftRecipies[selected_item][i][0]))
         except: col.number_input(f"Enter price of {sts.craftRecipies[selected_item][i][0]}", key=sts.craftRecipies[selected_item][i][0], step=1)
         num_of_inps += 1
     submittedCraft = st.form_submit_button('Submit')
@@ -31,7 +31,8 @@ craftSellPrice = st.number_input(f'Enter the sell price of {selected_item}')
 
 if submittedCraft:
 	running = 0
-	for i in (sts.craftRecipies[selected_item]):	
+	for i in (sts.craftRecipies[selected_item]):
+		sts.savedItems[i[0]] = int(getattr(sts, i[0]))
 		running += int(getattr(sts, i[0]))
 
 	st.write(running)
