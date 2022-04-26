41d8c7302e706a38b4f678b485a80e6de7376f0c
MasterMeep <51376656+MasterMeep@users.noreply.github.com>
2022-04-26 10:57:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ with st.form(key='columns_in_form'):
     cols = st.columns(len(sts.craftRecipies[selected_item]))
     for i, col in enumerate(cols):
         col.number_input(f"Enter price of {sts.craftRecipies[selected_item][i]}", key=f"craft_inp_{i}")
-    submitted = st.form_submit_button('Submit')
-	
-print(sts.craft_inp_0)
+    submittedCraft = st.form_submit_button('Submit')
+
+if submittedCraft:
+	print(sts.craft_inp_0)
 selected_item = st.selectbox('Select An Item To Refine', sts.refineItems)
